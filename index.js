2d06107dec1c1d50d46b2d82498c840c82cb2063
venkat sai ram <109469065+venkat2023@users.noreply.github.com>
2022-10-26 19:02:59 +0530

    update few things

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-function getRandPass(){
-    let pass=""
-    for(let i=0;i<15;i++){
-        pass+=characters[Math.floor(Math.random()*characters.length)]
-    }
-    return pass
+function getRandPass() {
+  let pass = "";
+  for (let i = 0; i < 15; i++) {
+    pass += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return pass;
 }
 
-function answer(){
-    const passwordOne = getRandPass()
-    const passwordTwo = getRandPass()
-    document.getElementById("pass1").textContent=passwordOne
-    document.getElementById("pass2").textContent=passwordTwo
+function answer() {
+  const passwordOne = getRandPass();
+  const passwordTwo = getRandPass();
+  document.getElementById("pass1").textContent = passwordOne;
+  document.getElementById("pass2").textContent = passwordTwo;
 }
---
+++ readme.md
@@ -0,0 +1,11 @@
+## To do
+
+1. ability to set the password length
+2. Add “copy-on-click”
+3. Toggle “symbols” and “numbers” on/off
+4. Make it responsive
+
+## Learnings
+
+1. properties like font or color are inherited by child elements (not form elements).
+2. 
