e7f9a0eef7ea80bde1dbdd3d0684a25aba06ec30
Venkat Sai Ram <heliobvsr2002@gmail.com>
2022-10-29 04:10:30 +0530

    update

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+const pass1Btn = document.getElementById("pass1");
+const pass2Btn = document.getElementById("pass2");
+const genPass = document.querySelector(".genpass");
+
 function getRandPass() {
   let pass = "";
   for (let i = 0; i < 15; i++) {
@@ -9,9 +13,18 @@ function getRandPass() {
   return pass;
 }
 
-function answer() {
+genPass.addEventListener("click", function () {
   const passwordOne = getRandPass();
   const passwordTwo = getRandPass();
   document.getElementById("pass1").textContent = passwordOne;
   document.getElementById("pass2").textContent = passwordTwo;
-}
+});
+
+pass1Btn.addEventListener("click", function () {
+  navigator.clipboard.writeText(pass1Btn.value);
+  alert("Copied the text: " + pass1Btn.value);
+});
+pass2Btn.addEventListener("click", function () {
+  navigator.clipboard.writeText(pass2Btn.value);
+  alert("Copied the text: " + pass2Btn.value);
+});
--- readme.md
+++ readme.md
@@ -3,10 +3,9 @@
 1. ability to set the password length
 2. Add “copy-on-click”
 3. Toggle “symbols” and “numbers” on/off
-4. Make it responsive
+4. Make it responsive(mobile view) probably try to add padding to the conatiner instead of adding padding to every element
 
 ## Learnings
 
 1. properties like font or color are inherited by child elements (not form elements).
-2. hello
-3. change 1
+2. [copy to clipboard](https://stackabuse.com/how-to-copy-to-clipboard-in-javascript-with-the-clipboard-api/)
